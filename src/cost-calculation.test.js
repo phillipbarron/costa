e64dd83ffb68a946be2d07b4b6743d7312f807a3
Phill Barron <Phill.Barron@bbc.co.uk>
2019-05-30 12:11:58 +0100

    added failing test

--- src/cost-calculation.test.js
+++ src/cost-calculation.test.js
@@ -2,6 +2,6 @@ const costCalulator = require('./index');
 
 describe('cost calculation', () => {
     it('adds stuff up right', () => {
-        expect(costCalulator.calculateCost(.023, 12)).toBe(1);
+        expect(costCalulator.calculateCost(1, 5)).toBe(15);
     });
 });
