94f58349c695770621341446d3be3ff5c6efd9f3
Phill Barron <Phill.Barron@bbc.co.uk>
2019-05-30 12:39:28 +0100

    expanded test coverage

--- src/cost-calculation.test.js
+++ src/cost-calculation.test.js
@@ -8,7 +8,18 @@ describe('cost calculation', () => {
         expect(costCalulator.calculateCost(0.2, 5)).toBe(3);
     });
     it('errors where arguments are non-numerical', () => {
-        expect(() => costCalulator.calculateCost([], "foo")).toThrow('non-numerical argument supplied');
+        const invalidArguments = [
+            { rate: 1, months: {} },
+            { rate: 1, months: [] },
+            { rate: 1, months: 'foo' },
+            { rate: {}, months: 1 },
+            { rate: [], months: 1 },
+            { rate: 'foo', months: 1 }
+        ]
+        invalidArguments.forEach(invalidArgument => {
+            expect(() => costCalulator.calculateCost(invalidArgument.rate, invalidArgument.months)).toThrow('non-numerical argument supplied');
+        });
+
     });
     // it('adds floats correctly', () => {
     //     expect(costCalulator.calculateCost(0.2, 5)).toBe(3);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const calculateCost = (rate, months) => {
     [rate, months].forEach(arg => {
-        if(isNaN(arg)) throw new Error('non-numerical argument supplied')
+        if(isNaN(arg) || Array.isArray(arg)) throw new Error('non-numerical argument supplied')
     });
     let result = 0;
     for(let i = 0; i <= months; i += 1) {
