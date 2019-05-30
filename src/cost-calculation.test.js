d06f04af25cc56a497cfe6b254d47febc5443b43
Phill Barron <Phill.Barron@bbc.co.uk>
2019-05-30 12:19:08 +0100

    passing test for floats

--- src/cost-calculation.test.js
+++ src/cost-calculation.test.js
@@ -1,7 +1,10 @@
 const costCalulator = require('./index');
 
 describe('cost calculation', () => {
-    it('adds stuff up right', () => {
+    it('adds whole numbers correctly', () => {
         expect(costCalulator.calculateCost(1, 5)).toBe(15);
+    });
+    it('adds floats correctly', () => {
+        expect(costCalulator.calculateCost(0.2, 5)).toBe(3);
     });
 });
