c1e00e0ed79a1ba54302e69fa0e58ad39716c138
Phill Barron <Phill.Barron@bbc.co.uk>
2019-05-30 12:22:08 +0100

    added failing test

--- src/cost-calculation.test.js
+++ src/cost-calculation.test.js
@@ -7,4 +7,10 @@ describe('cost calculation', () => {
     it('adds floats correctly', () => {
         expect(costCalulator.calculateCost(0.2, 5)).toBe(3);
     });
+    it('errors where arguments are non-numerical', () => {
+        expect(costCalulator.calculateCost([], "foo")).toThrow('non-numerical argument supplied');
+    });
+    // it('adds floats correctly', () => {
+    //     expect(costCalulator.calculateCost(0.2, 5)).toBe(3);
+    // });
 });
